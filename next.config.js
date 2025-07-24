881fb425feb97d838e4ad47e5b3987a2c62dd836
wyz3r <leonelislasglez@gmail.com>
2025-07-23 22:18:40 -0600

    Fix next.config.js: remove deprecated experimental.appDir

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
